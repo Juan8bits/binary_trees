495aa47fd77deeaf065642c438db328c4b7997c6
Juan8bits <juan160820@gmail.com>
2021-03-08 23:32:02 +0000

    Remove Return value in the function description

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -2,7 +2,6 @@
 /**
  * binary_tree_delete - function that deletes an entire binary tree.
  * @tree: Is a pointer to the root node of the tree to delete.
- * Return:
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
